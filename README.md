81e88a1ee41d78e24d1d9102914c4ec0d03c08cc
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-12-10 09:57:05 +0000

    reading

---
+++ README.md
@@ -0,0 +1,5 @@
+## Generator.prototype.next()
+
+The `next()` method returns an object with two properties `done` and `value`. You can also provide a parameter to the `next` method to send a value to the generator.
+
+* [Generator.prototype.next()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator/next)
--- yield-is-a-two-way-street/hello-entry-argument.js
+++ yield-is-a-two-way-street/hello-entry-argument.js
@@ -1,12 +1,13 @@
-function* generator() {
-    let a = yield 1;
-    console.log('Inside generator: '+a); // hello
+function* generator(z) {
+    console.log(z); // 999
+    let a = yield z+1;
+    console.log('Inside generator: '+a); // a is hello
     let b = yield (a+" world!");
-    console.log('Inside generator: '+b); // 10
+    console.log('Inside generator: '+b); // b is 10
     yield b*2;
 }
 
-let g = generator();
-console.log(g.next().value); // 1
-console.log(g.next("hello").value); // hello world!
+let g = generator(999);
+console.log(g.next().value); // 1000
+console.log(g.next("hello", "second parameter").value); // hello world!
 console.log(g.next(10).value); // 20
