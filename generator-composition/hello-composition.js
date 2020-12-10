c5524d53e02c7d23770fd6b7d2e3cb6d0d79d6fc
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-12-10 09:00:28 +0000

    reading

---
+++ generator-composition/hello-composition.js
@@ -0,0 +1,27 @@
+// The yield* directive delegates the execution to another generator. 
+// This term means that yield* gen iterates over the generator gen 
+// and transparently forwards its yields outside. 
+// As if the values were yielded by the outer generator.
+
+function* mygen(first, last) {
+    for(let i = first; i <= last; i++) yield i;
+}
+
+function* composed() {
+    yield* mygen('0'.charCodeAt(0), '9'.charCodeAt(0));
+    yield* mygen('A'.charCodeAt(0),'Z'.charCodeAt(0));
+    yield* mygen('a'.charCodeAt(0),'z'.charCodeAt(0))
+}
+
+String.prototype.char = function(i) { return [...this][i]; }
+
+console.log([...composed()].map(code => String.fromCharCode(code)).join(''));
+
+
+let str = '';
+
+for(let code of composed()) {
+  str += String.fromCharCode(code);
+}
+
+console.log(str);
