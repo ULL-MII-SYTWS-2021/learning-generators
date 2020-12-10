c7130aa1a583ee79ffcf745b18ea8e38e81a052d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-12-10 08:05:47 +0000

    reading

--- 0-symbol-iterator/hello-symbol-iterator.js
+++ 0-symbol-iterator/hello-symbol-iterator.js
@@ -1,19 +1,23 @@
+const z = "hello";
 const range = {
     from: 4,
-    to: 10
-}
-
-range[Symbol.iterator] = function() {
-    return {
-        current: this.from,
-        last: this.to,
-        next() {
-            if (this.current <= this.last) 
-              return { done: false, value: this.current++}
-            else return { done: true, value: undefined }
+    to: 10,
+    [z]: "world!", // See how it works the [] notation inside an object
+    [Symbol.iterator]: function() {
+        return {
+            current: this.from,
+            last: this.to,
+            next() {
+                if (this.current <= this.last) 
+                  return { done: false, value: this.current++}
+                else return { done: true, value: undefined }
+            }
         }
     }
+    
 }
+
+console.log(range.hello)
 
 for (let e of range) {
     console.log(e);
