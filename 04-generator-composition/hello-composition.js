4cbef34ba6ea5aea9e56ba0f0f366c8f48f26d02
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-16 13:59:43 +0100

    reading

--- 04-generator-composition/hello-composition.js
+++ 04-generator-composition/hello-composition.js
@@ -4,25 +4,24 @@
 // As if the values were yielded by the outer generator.
 
 function* mygen(first, last) {
-    for(let i = first; i <= last; i++) yield i;
+  for (let i = first; i <= last; i++) yield i;
 }
 
+// Generates all three character combinations of numbers, uppercase letters, and lowercase letters.
 function* composed() {
-    yield* mygen('0'.charCodeAt(0), '9'.charCodeAt(0));
-    yield* mygen('A'.charCodeAt(0),'Z'.charCodeAt(0));
-    yield* mygen('a'.charCodeAt(0),'z'.charCodeAt(0))
+  for(const num of mygen(48, 57)) {
+    for (const upper of mygen(65, 90)) {
+      for (const lower of mygen(97, 122)) {
+        yield [num, upper, lower].map(String.fromCharCode).join("");
+    }
+    }
+  }
 }
 
-let res = [...composed()]
-          .map(code => String.fromCharCode(code))
-          .join('');
-console.log(res);
-// 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
-
-let str = '';
-
-for(let code of composed()) {
-  str += String.fromCharCode(code);
+// get a random code from the composed generator
+function randomCode() {
+  const codes = [...composed()];
+  return codes[Math.floor(Math.random() * codes.length)];
 }
 
-console.log(str);
+console.log(randomCode());
