fa79303bef2c8ae99dad12d431dcf06ca1d4a8a6
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-16 14:36:56 +0100

    reading

--- 07-generator-throw/hello-throw.js
+++ 07-generator-throw/hello-throw.js
@@ -1,10 +1,10 @@
 function* gen() {
     try {
       let result = yield "2 + 2 = ?"; // (1)
-  
       console.log("The execution does not reach here, because the exception is thrown above");
     } catch(e) {
       console.log(e.message); // shows the error
+      //yield 4; // if uncommented will yield { value: 4, done: false }
     }
   }
   
@@ -12,5 +12,5 @@ function* gen() {
   
   let question = generator.next().value;
   
-  let r = generator.throw(new Error("The answer is not found in my database")); // (2)
+  let r = generator.throw(new Error("The answer is not found")); 
   console.log(r); // { value: undefined, done: true }
