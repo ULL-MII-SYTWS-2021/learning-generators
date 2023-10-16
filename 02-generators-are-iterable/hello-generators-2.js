6a7c22978baa7ab65f886baefc2b8ae9f06c60a2
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-16 12:57:02 +0100

    reading

--- 02-generators-are-iterable/hello-generators-2.js
+++ 02-generators-are-iterable/hello-generators-2.js
@@ -6,6 +6,10 @@ function* yes() {
 
 let g = yes();
 console.log(g);
+/*
+Note: the example below shows 1, then 2, and it doesn’t show 3!
+It’s because for..of iteration ignores the last value, when done: true.
+*/
 for(let x of g) {
     console.log(x);
 }
