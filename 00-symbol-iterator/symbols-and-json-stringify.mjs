80e53632e39763a64dcd1399d5da2e9dd206325d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-16 13:07:12 +0100

    reading

--- 00-symbol-iterator/symbols-and-json-stringify.mjs
+++ 00-symbol-iterator/symbols-and-json-stringify.mjs
@@ -1,5 +1,6 @@
 /*
-Symbols are also convenient for private properties because they do not show up in JSON.stringify() output. 
+Symbols are also convenient for private properties because 
+they do not show up in JSON.stringify() output. 
 Specifically, JSON.stringify() silently ignores symbol keys and values.
 */
 const symbol = Symbol('test');
