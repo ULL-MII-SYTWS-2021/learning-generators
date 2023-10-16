4f2229097e4bf3280195f05d0764122690d25bec
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-16 13:02:23 +0100

    reading

--- 02-generators-are-iterable/spread.js
+++ 02-generators-are-iterable/spread.js
@@ -1,7 +1,9 @@
+// As generators are iterable, we can call all related functionality, 
+// e.g. the spread syntax ... 
 function* generator() {
     yield 1;
     yield 2;
     yield 3;
 }
 
-console.log([...generator()]);
+console.log([-1, 0, ...generator(), 4]);
