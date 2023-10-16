252991321ee9e08ff558bd9b39c5b1f2a37c5f6f
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-16 14:43:44 +0100

    reading

--- 07-generator-throw/hello-throw.js
+++ 07-generator-throw/hello-throw.js
@@ -1,16 +1,16 @@
 function* gen() {
-    try {
-      let result = yield "2 + 2 = ?"; // (1)
-      console.log("The execution does not reach here, because the exception is thrown above");
-    } catch(e) {
-      console.log(e.message); // shows the error
-      //yield 4; // if uncommented will yield { value: 4, done: false }
-    }
+  try {
+    let result = yield "2 + 2 = ?"; // (1)
+    console.log("The execution does not reach here, because the exception is thrown above");
+  } catch (e) {
+    console.log(e.message); // shows the error
+    //yield 4; // if uncommented will yield { value: 4, done: false }
   }
-  
-  let generator = gen();
-  
-  let question = generator.next().value;
-  
-  let r = generator.throw(new Error("The answer is not found")); 
-  console.log(r); // { value: undefined, done: true }
+}
+
+let generator = gen();
+
+let question = generator.next().value;
+
+let r = generator.throw(new Error("The answer is not found"));
+console.log(r); // { value: undefined, done: true }
