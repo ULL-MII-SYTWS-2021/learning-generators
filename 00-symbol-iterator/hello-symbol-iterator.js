3d543b75f136ce626db2d0821129dc84734ef7b4
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-16 10:17:56 +0100

    reading

--- 00-symbol-iterator/hello-symbol-iterator.js
+++ 00-symbol-iterator/hello-symbol-iterator.js
@@ -6,18 +6,18 @@ const range = {
     to: 10,
     [w]: "hello",
     [z]: "world!", // See how it works the [] notation inside an object
-    [Symbol.iterator]: function() {
+    [Symbol.iterator]: function () {
         return {
             current: this.from,
             last: this.to,
             next() {
-                if (this.current <= this.last) 
-                  return { done: false, value: this.current++}
+                if (this.current <= this.last)
+                    return { done: false, value: this.current++ }
                 else return { done: true, value: undefined }
             }
         }
     }
-    
+
 }
 
 console.log(range.hello); // world!
@@ -25,7 +25,7 @@ console.log(range[w]); // range.w does not work!
 
 console.log("for ... of ...")
 for (let e of range) {
-    for(let e2 of range) {
+    for (let e2 of range) {
         console.log(e, e2);
     }
 }
@@ -35,7 +35,14 @@ console.log("Explicitly:");
 let iterator = range[Symbol.iterator]();
 
 while (true) {
-  let result = iterator.next();
-  if (result.done) break;
-  console.log(result.value); 
+    let result = iterator.next();
+    if (result.done) break;
+    console.log(result.value);
+}
+
+// Infinite iterators are also possible.
+range.to = Infinity;
+for (const i of range) {
+    console.log(i);
+    if (i >= 12) break;
 }
