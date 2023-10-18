dbe091028c21ed8e6dc80f7e693a801ecc7cbfc3
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-18 15:38:12 +0100

    reading

--- 07-generator-throw/hello-throw.js
+++ 07-generator-throw/hello-throw.js
@@ -1,16 +1,19 @@
+/* To pass an error into a yield, we should call generator.throw(err). 
+   In that case, the err is thrown in the line with that yield.
+*/
 function* gen() {
   try {
-    let result = yield "2 + 2 = ?"; // (1)
+    let result = yield "2 + 2 = "; 
     console.log("The execution does not reach here, because the exception is thrown above");
   } catch (e) {
     console.log(e.message); // shows the error
-    //yield 4; // if uncommented will yield { value: 4, done: false }
+    // yield 4; 
   }
 }
 
 let generator = gen();
 
 let question = generator.next().value;
-
+process.stdout.write(question);
 let r = generator.throw(new Error("The answer is not found"));
-console.log(r); // { value: undefined, done: true }
+console.log(r); 
